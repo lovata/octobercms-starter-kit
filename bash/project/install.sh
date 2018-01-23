fac5e98dc4a0d09739a998a78a12624668aec31a
Pavel Lautsevich <zigzag.mcquack@gmail.com>
2018-01-23 14:09:19 +0300

    Extract Readme backup script to the external file

--- bash/project/install.sh
+++ bash/project/install.sh
@@ -35,22 +35,8 @@ if $CHECK_PASSED; then
         bash $CFG_PATH/delete-git.sh $CFG_PATH
 
         # Backup Starter Kit README file
-        sleep 0.5
-        echo
-        echo -e "\e[7m       BACKUP STARTER KIT README (STEP 3/7)       \e[0m"
-        echo
-        README_FILE="./README.md"
-        sleep 0.5
-        if [[ -e $README_FILE ]]; then
-            mv ./README.md ./README.STARTERKIT.md
-            echo
-            echo -e "\e[32m✓ OK!\e[0m"
-            echo
-        else
-            echo
-            echo -e "\e[34m🛈  Nothing to rename!\e[0m"
-            echo
-        fi
+        bash $CFG_PATH/readme-backup.sh $CFG_PATH
+
 
         bash $CFG_PATH/october.sh $CFG_PATH
         bash $CFG_PATH/webpack.sh $CFG_PATH
