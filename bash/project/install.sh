fa72ba0848a463d15ba15ca7fa6d17bb63ea79eb
Pavel Lautsevich <zigzag.mcquack@gmail.com>
2018-01-23 13:26:17 +0300

    Add config checking

--- bash/project/install.sh
+++ bash/project/install.sh
@@ -1,14 +1,34 @@
 #!/usr/bin/env bash
 
+# Getting the path of the executable script
+CFG_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. $CFG_PATH/config.cfg
+
+# Checking settings variables initialization
+echo
+echo -e "\e[7m          CHECK PROJECT SETTINGS (STEP 1/7)          \e[0m"
+echo
+
+CHECK_PASSED=true
+
+sleep 0.5
+for VARIABLE in PROJECT_NAME PROJECTS_DIR VIRTUALHOST_NAME DB_NAME DB_USER DB_PASSWORD DB_CHARACTER_SET DB_COLLATION OCTOBER_GIT_INSTALL
+do
+    if [ -n "${!VARIABLE}" ]; then
+        echo -e "\e[32m✓ \e[3m$VARIABLE is defined as \e[39m\e[3m${!VARIABLE}\e[0m"
+    else
+        # sleep 0.5
+        echo -e "\e[31m❌ \e[3m$VARIABLE is not defined!\e[0m"
+        CHECK_PASSED=false
+    fi
+done
+
 GIT_DIRECTORY=/vagrant/.git
 echo $GIT_DIRECTORY
 if [ -d "$GIT_DIRECTORY" ]; then
     echo "Delete Starter Kit Git directory…"
     rm -rf ./.git
 fi
-
-echo "Reading config…"
-CFG_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "Backuping README file of Starter kit…"
 mv ./README.md ./README.STARTERKIT.md
