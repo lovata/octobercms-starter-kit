41cafb90e4b7c0757f528626c4d406f44f0537b7
Pavel Lautsevich <p.lovtsevich@lovata.com>
2018-06-15 20:01:04 +0300

    Remove Vagrant env var check

--- bash/environment/install.sh
+++ bash/environment/install.sh
@@ -2,15 +2,11 @@
 
 sudo apt-get update
 
-if $VAGRANT; then
-    echo "Install in Vagrant environment…"
-    bash /vagrant/bash/environment/zipunzip.sh
-    bash /vagrant/bash/environment/apache.sh
-    bash /vagrant/bash/environment/curl.sh
-    bash /vagrant/bash/environment/php.sh
-    bash /vagrant/bash/environment/composer.sh
-    bash /vagrant/bash/environment/mysql.sh
-    bash /vagrant/bash/environment/nodejs.sh
-else
-    echo "Install in local environment…"
-fi
+echo "Install in Vagrant environment…"
+bash /vagrant/bash/environment/zipunzip.sh
+bash /vagrant/bash/environment/apache.sh
+bash /vagrant/bash/environment/curl.sh
+bash /vagrant/bash/environment/php.sh
+bash /vagrant/bash/environment/composer.sh
+bash /vagrant/bash/environment/mysql.sh
+bash /vagrant/bash/environment/nodejs.sh
